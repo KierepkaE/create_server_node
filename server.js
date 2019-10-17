e41eee8889570eadfbe64841ac61812798a94db5
kierepkae <kierepkaedyta@gmail.com>
2019-10-17 19:05:33 +0100

    rewrite routing for / and /users

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
+const path = require('path');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-  console.log(req.url)
-  console.log(req.method)
-
-  if (req.url === '/') {
-
-    res.end(`
-    <h1>Main</h1>
-    <h2>Path:  ${req.url}</h2>`);
-  } else if (req.url === '/users') {
-
-    res.end(`
-    <h1>Users</h1>
-    <h2>Path:  ${req.url}</h2>`);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' })
-    res.end(`
-    <h2>Upss. This adress ${req.url} is not correct.</h2>`);
-
+  switch (req.url) {
+    case '/':
+      fs.readFile((path.join(__dirname), 'index.html'), (err, page) => {
+        if (err) res.end('<h1>Something went wrong.</h1>');
+        else res.end(page);
+      });
+      break;
+    case '/users':
+      fs.readFile((path.join(__dirname), 'users.html'), (err, page) => {
+        if (err) res.end('<h1>Something went wrong.</h1>');
+        else res.end(page);
+      });
+      break;
+    case '/api/users':
+      res.end('API path')
+      break;
+    default:
+      res.end('DEFAULT')
+      break;
   }
-}).listen(port, '127.0.0.1', () => {
-  console.log('Listening on ... ' + port);
-});
+}).listen(port, '127.0.0.1');
