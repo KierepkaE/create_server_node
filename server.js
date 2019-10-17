00e11ec9e6a2a2ae62ea826558a2eecf0e1ea1c8
kierepkae <kierepkaedyta@gmail.com>
2019-10-17 18:14:39 +0100

    add response Head

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("<h1>node</h1>");
+  res.end();
 }).listen(3000);
